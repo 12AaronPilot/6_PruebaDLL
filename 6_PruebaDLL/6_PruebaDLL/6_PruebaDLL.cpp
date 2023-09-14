ff97afceefe9b4905e365a8e7a4b31e4801d54b3
12AaronPilot <134745095+12AaronPilot@users.noreply.github.com>
2023-09-13 21:18:08 -0600

    Add files via upload
    Este paso lo mismo, no pude hacer push...
    Asi que lo hice por aqui :)

---
+++ 6_PruebaDLL/6_PruebaDLL/6_PruebaDLL.cpp
@@ -0,0 +1,68 @@
+// 6_PruebaDLL.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+
+int main()
+{
+    puts("Cargando  DLL");
+    //Cargar la dll
+    HINSTANCE mydll = LoadLibrary(L"6_dll.dll");
+
+    //Comprobar si cargó la dll
+    if (mydll != NULL)
+    {
+        puts("Se cargo la DLL :)");
+        typedef const char* (*GetCodenameFunc)();
+        GetCodenameFunc getcodename = reinterpret_cast<GetCodenameFunc>(GetProcAddress(mydll, "GetCodename"));
+        if (getcodename != NULL)
+        {
+            const char* mensaje = getcodename();
+            std::cout << "desde la DLL" << mensaje << "\n";
+        }
+        else
+        {
+            puts("No se encontro la funcion :(");
+        }
+
+
+        //Cargar la funcion sumar
+        typedef double (*sumarFunc)(double a, double b);
+        sumarFunc sumar = reinterpret_cast<sumarFunc>(GetProcAddress(mydll, "sumar"));
+        if (sumar != NULL)
+        {
+            std::cout << "prueba de suma:" << sumar (3.1416, 1.414);
+            std::cout << "\n";
+        }
+        //Concatenacion
+        typedef std::string(*ConcatenarFunc)(const std::string& A, const std::string& B);
+        ConcatenarFunc concatenar = reinterpret_cast<ConcatenarFunc>(GetProcAddress(mydll, "Concatenar"));
+        if (concatenar != NULL)
+        {
+            std::cout << "Concatenacion: " << concatenar("Sooooy, ", "Franchescooo...") << "\n";
+        }
+        else
+        {
+            puts("No se encontro la funcion");
+        }
+        FreeLibrary(mydll);
+    }
+    else
+    {
+        puts("No se pudo cargar la DLL :(");
+    }
+
+
+    //Concatenacion de strings
+    std::string string1 = "Hola soy";
+    std::string string2 = "AaronMP";
+
+    std::string s = string1 + " " + string2;
+
+    std::cout << s << std::endl;
+
+
+    return 0;
+}
